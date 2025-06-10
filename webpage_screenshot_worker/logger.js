5a69c75547d67807695e768a04640f5db78ab757
yuhaonan <v-yuhaonan@sinochem.com>
2025-06-10 17:59:49 +0800

    日志格式

--- webpage_screenshot_worker/logger.js
+++ webpage_screenshot_worker/logger.js
@@ -6,8 +6,18 @@ import { dirname } from 'node:path'
 // Set this to whatever, by default the path of the script.
 const logPath = path.join(dirname(fileURLToPath(import.meta.url)), 'logs');
 const tsFormat = () => (new Date().toISOString());
-    
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
 const log = winston.createLogger({
+    format: combine(
+        timestamp(), 
+        myFormat 
+    ),
     transports: [
         new winston.transports.File({
             filename: path.join(logPath, 'ws.log'),
